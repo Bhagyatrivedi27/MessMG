fdc54066b99d5ce2df9dc74a02ba32c1b48adfe9
Bhagyatrivedi27 <53113456+Bhagyatrivedi27@users.noreply.github.com>
2020-10-03 14:38:53 +0530

    Delete posts.js

--- routes/posts.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const verifyT = require('./verifyToken');
-const user = require('../model/user');
-
-router.get('/', verifyT, (req, res) => {
-  res.send(req.user);
-});
-module.exports = router;
